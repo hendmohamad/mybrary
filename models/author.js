f95a3c0c4236e6134566dff261c1d809bbacb650
hend.mohamed <hend.mohamed@gas.mercedes-benz.com.eg>
2019-12-12 15:44:00 +0200

    Initial Author Routes

---
+++ models/author.js
@@ -0,0 +1,11 @@
+const mangoose = require('mongoose');
+
+const authorSchema = new mangoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mangoose.model('Author', authorSchema);
--- server.js
+++ server.js
@@ -6,13 +6,16 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
+const authorRourser = require('./routes/author');
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}));
 
 const mongoose = require('mongoose');
 //console.log('URL:' +process.env.DATABASE_URL);
@@ -22,4 +25,5 @@ db.on('error', error => console.error(error));
 db.on('open', () => console.log('CONNECTED TO MANGOOSE'))
 
 app.use('/', indexRouter);
+app.use('/authors', authorRourser);
 app.listen(process.env.PORT || 8000);
